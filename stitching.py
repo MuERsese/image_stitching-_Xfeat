3bc85700c6e5d012f7f8796aaff1f79daa2057a3
bobguo <gab040510@gmail.com>
2025-01-06 20:06:35 +0800

    Correct_output_size

--- stitching.py
+++ stitching.py
@@ -11,62 +11,48 @@ class stitching:
 
         key_frame_homo=[]
         key_frame_homo.append(key_frame[0])
-        #first we need to initialize to set up the value of min and max
-        init_h,init_w,_=key_frame[0].shape
-        min_x=0
-        max_x=init_w
-        min_y=0
-        max_y=init_h
 
-        #first calculate all homography matrix
+        #we assume that all images have the same shape, so their corners are the same
+        h,w,_=key_frame[0].shape
+        
+        transformed_corners=[]
+        corners = np.float32([[0, 0], [w, 0], [0, h], [w, h]]).reshape(-1, 1, 2)
+        
+
+        #first calculate all homography matrix,and store the transformed corners
         for i in range(1,n):
             homography[i]=homography[i].dot(homography[i-1])
+            transformed_corners.append(cv2.perspectiveTransform(corners, homography[i]))
             
-            
-        #create an empty canva for output
-        #first define the size of output
-        h, w ,_= key_frame[0].shape
-        corners = np.float32([[0, 0], [w, 0], [0, h], [w, h]]).reshape(-1, 1, 2)
-
-        # calculate the perspective corners
-        #suppose lats image is the right most image
-        transformed_corners = cv2.perspectiveTransform(corners, homography[-1])
-        
-        
+        #now we need to find xmin,xmax,ymin,ymax
        
+        xmin_output=int(np.min(np.array(transformed_corners)[:,:,:,0]))
+        xmax_output=int(np.max(np.array(transformed_corners)[:,:,:,0]))
+        ymin_output=int(np.min(np.array(transformed_corners)[:,:,:,1]))
+        ymax_output=int(np.max(np.array(transformed_corners)[:,:,:,1]))
+
+        #now calculate the coordinate homography matrix
+        coor_homo=np.array([[1,0,xmin_output],[0,1,ymin_output],[0,0,1]])
+
+        for j in range (n):
+            homography[i]=coor_homo.dot(homography[i])
         
-        
-        output_H = max(max_x,int(np.max(transformed_corners[:, 0, 0])))
-        output_W = max(max_y,int(np.max(transformed_corners[:, 0, 1])))
-        output=np.zeros((output_H,output_W,3),dtype=np.uint8)
-        #initialization of the first image
-        output[0:key_frame[0].shape[0],0:key_frame[0].shape[1]]=key_frame[0]
+        #create an empty canva for the output
+        output=np.zeros((ymax_output-ymin_output,xmax_output-xmin_output),dtype=np.uint8)
+
+        #clculate the perspective image and then stitch them to the output
+        # for k in range(1,n):
+        #     #we need to decide dsize(the w and h of the output frame)
+
+
+        #     key_frame_homo=cv2.warpPerspective(key_frame[i],homography[i],dsize)
 
 
 
-        for j in range(1,n):
-            # Get the original dimensions of the key frame
-            
 
-            # Define the corners of the image
-            corners = np.float32([[0, 0], [w, 0], [0, h], [w, h]]).reshape(-1, 1, 2)
 
-            # calculate the perspective corners
-            transformed_corners = cv2.perspectiveTransform(corners, homography[j])
 
-            
-            min_x = min(min_x,int(np.min(transformed_corners[:, 0, 0])))
-            max_x = max(max_x,int(np.max(transformed_corners[:, 0, 0])))
-            min_y = min(min_y,int(np.min(transformed_corners[:, 0, 1])))
-            max_y = max(max_y,int(np.max(transformed_corners[:, 0, 1])))
-
-            # Calculate width and height of output
-            width = max_x - min_x
-            height = max_y - min_y
-            dsize = (width, height)
-            
-            
-            output[min_y:height+min_y,min_x:width+min_x]=cv2.warpPerspective(key_frame[i],homography[i],dsize)
+           
 
         
         return output
