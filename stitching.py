e35438a5912116a58cec14521d2f2cac9d16a0b2
bobguo <gab040510@gmail.com>
2025-01-06 21:37:25 +0800

    70%complete

--- stitching.py
+++ stitching.py
@@ -9,8 +9,7 @@ class stitching:
             print("no keyframes detected")
             return None
 
-        key_frame_homo=[]
-        key_frame_homo.append(key_frame[0])
+    
 
         #we assume that all images have the same shape, so their corners are the same
         h,w,_=key_frame[0].shape
@@ -31,30 +30,35 @@ class stitching:
         ymin_output=int(np.min(np.array(transformed_corners)[:,:,:,1]))
         ymax_output=int(np.max(np.array(transformed_corners)[:,:,:,1]))
 
-        #now calculate the coordinate homography matrix
-        coor_homo=np.array([[1,0,xmin_output],[0,1,ymin_output],[0,0,1]])
-
-        for j in range (n):
-            homography[i]=coor_homo.dot(homography[i])
-        
         #create an empty canva for the output
-        output=np.zeros((ymax_output-ymin_output,xmax_output-xmin_output),dtype=np.uint8)
-
-        #clculate the perspective image and then stitch them to the output
-        # for k in range(1,n):
-        #     #we need to decide dsize(the w and h of the output frame)
-
-
-        #     key_frame_homo=cv2.warpPerspective(key_frame[i],homography[i],dsize)
-
-
-
-
-
-
-           
+        output=np.zeros((ymax_output-ymin_output,xmax_output-xmin_output,3),dtype=np.uint8)
 
         
+        #now calculate the coordinate homography matrix
+        if(xmin_output<0):
+            xmin_output=-xmin_output
+        if(ymin_output<0):
+            ymin_output=-ymin_output
+        coor_homo=np.array([[1,0,xmin_output],[0,1,ymin_output],[0,0,1]],dtype=np.uint8)
+
+    
+        
+        
+        #clculate the perspective image and then stitch them to the output
+        for k in range(n-1,0,-1):
+            #we need to decide dsize(the w and h of the output frame)
+            #we need to recalculate all corners,because the homography changed
+            
+            corners_cur=cv2.perspectiveTransform(corners,coor_homo.dot(homography[k]))
+            
+            
+            [xmin,ymin]=np.int32(corners_cur.min(axis=0).ravel())
+            [xmax,ymax]=np.int32(corners_cur.max(axis=0).ravel())
+            dsize=(xmax-xmin,ymax-ymin)
+            key_frame_homo_=cv2.warpPerspective(key_frame[k],coor_homo.dot(homography[k]),dsize)
+
+            #stitch the image to the output
+            output[0:ymax-ymin,0:xmax-xmin]=key_frame_homo_
         return output
 
         
